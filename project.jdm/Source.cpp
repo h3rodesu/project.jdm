5c9f00a69f2f43ef5fd4df4c01e3604de6df5020
h3rodesu <NikaPyshkinNika@gmail.com>
2026-06-07 23:14:44 +0300

    Update tuning shop/Lists
    Обновил систему тюнинга с помощью связных списков

--- project.jdm/Source.cpp
+++ project.jdm/Source.cpp
@@ -24,7 +24,7 @@ public:
 	 wheel=zacep;
 	}
 	void show() {
-		cout << "Модель:" << carName << "\nМарка:" << carBrand << "\nМощность:" << ls << " Лошадиных сил" << "\nВес:" << kg << " Килограмм" <<"\nЗацеп"<<wheel << endl;
+		std::cout << "Модель:" << carName << "\nМарка:" << carBrand << "\nМощность:" << ls << " Лошадиных сил" << "\nВес:" << kg << " Килограмм" <<"\nЗацеп"<<wheel << endl;
 	}
 	void setLs(int value = 100) {
 		ls = ls + value;
@@ -68,17 +68,8 @@ public:
 
 	}
 };
-void light(vector<car>& lightest) {
-	car* morelight = &lightest[0];
-	for (int i = 0; i < lightest.size(); i++) {
-		if (lightest[i].getkg() < morelight->getkg()) {
-			morelight = &lightest[i];
-		}
-	}
-	cout <<"Самая лёгкая машина: "<< morelight->getname();
-}
 class doublyListNode {
-private:
+public:
 	struct Node {
 		car* carptr;
 		Node* next;
@@ -116,7 +107,7 @@ public:
 		Node* current = head;
 		while (current != nullptr) {
 			current->carptr->show();
-			cout << "--------------------------" << endl;
+			std::cout << "--------------------------" << endl;
 			current = current->next;
 		}
 	}
@@ -124,7 +115,7 @@ public:
 		Node* current=head;
 		Node* fastestNode = head;
 		while (current == nullptr) {
-			cout << "Машина не выбрана" << endl;
+			std::cout << "Машина не выбрана" << endl;
 			return;
 		}
 		while(current!=nullptr){
@@ -133,14 +124,14 @@ public:
 			}
 			current = current->next;
 		}
-		cout << "Самая мощная машина" << fastestNode->carptr->getname();
+		std::cout << "Самая мощная машина" << fastestNode->carptr->getname();
 
 	}
 	void lightest() {
 		Node* current = head;
 		Node* light = head;
 		while (current == nullptr) {
-			cout << "Машина не вырбрана" << endl;
+			std::cout << "Машина не выбрана" << endl;
 			return;
 		}
 		while (current != nullptr) {
@@ -149,12 +140,93 @@ public:
 			}
 			current = current->next;
 		}
-		cout << "Самая легкая машина: " << light->carptr->getname() << endl;
+		std::cout << "Самая легкая машина: " << light->carptr->getname() << endl;
+	}
+
+	Node* getCarNode(int choice) {
+		Node* current = head;
+		int count = 1;
+		while (current != nullptr) {
+			if (count == choice) {
+				return current;
+		}
+			count++;
+			current = current->next;
+		}
+
 	}
 };
+struct Part {//"Коробка" с грузом (деталями)
+	string partName;
+	int partCost;
+	int partBoost;
+	Part(string name, int cost, int bonus) : partName(name), partCost(cost), partBoost(bonus) {}
+};
+	class tuningShop {//Класс магазина
+	private:
+		struct shopNode {
+			shopNode* next;
+			shopNode* prev;
+			Part* partptr;
+			shopNode(Part* ptr) : partptr(ptr), next(nullptr),prev(nullptr) {}
+		};
+	public:
+		shopNode* head = nullptr;
+
+//Функция добавления запчастей в конец списка(с конца списка)
+	
+		void addToEnd(Part*detail) {
+			shopNode* newNode = new shopNode(detail);//new т.к. создается потеницально новый объект, 
+			//detail-временая переменная для пердачи данных в partptr;
+			shopNode* current = head;
+				if (head == nullptr) {
+					head = newNode;
+				}
+				else {
+					while (current->next != nullptr) {
+						current=current->next;
+					}
+						current->next=newNode;
+						newNode->prev = current;
+					}
+			}
+
+		//Функция для вывода магазина
+		void showShop() { 
+			shopNode* current = head;
+			int index = 1;
+			if (head == nullptr) {
+				std::cout << "На полках пусто" << endl;
+				return;
+			}
+			while (current!= nullptr) {
+				std::cout << index << "." << "Деталь:" << current->partptr->partName << " Стоимость: " << current->partptr->partCost << "Эффект от детали:" << current->partptr->partBoost << endl;
+				std::cout << endl;	
+				std::cout << setfill('-') << setw(20)<<endl;
+				current = current->next;
+				index++;
+			}
+		}
+
+		//Сверение выбора игрока с деталью из магазина 
+
+		Part*getPartPtr(int menuIndex) {
+			int count = 1;
+			shopNode* current = head;//Имеет тип shopNode
+			while (current	!= nullptr) {
+				if (menuIndex == count) {
+					return current->partptr;
+				}
+				count++;
+				current = current->next;
+		}
+			return nullptr;
+		}
+	};
 int main() {
 	srand(time(0));
 	setlocale(LC_ALL, "rus");
+	doublyListNode::Node* activeCarNode = nullptr;
 	Nissan silvia("Silvia s13", "Nissan", 240, 1110, 30);
 	Nissan skyline("Skyline", "Nissan", 240, 1430, 35);
 	Toyota mark("Mark II", "Toyota", 180, 1450, 70);
@@ -166,19 +238,20 @@ int main() {
 	int choice = 0;
 	bool shop = true;
 	while (shop) {
-		cout << "Выберете машину 1-3 или выйдите-0" << endl;
+		std::cout << "Выберете машину 1-3 или выйдите-0" << endl;
 		cin >> choice;
 		if (choice == 1) {
+		 activeCarNode = mygarage.getCarNode(choice);
 			mark.show();
-			cout << endl;
+			std::cout << endl;
 			int swap = 0;
 			int limit = 280;
 			if (mark.getls() < limit) {
-				cout << "Достпуен свап на 2jz.Замена?" << endl;
+				std::cout << "Достпуен свап на 2jz.Замена?" << endl;
 				cin >> swap;
 				if (swap == 1) {
 					mark.setLs();
-					cout << "Двигатель свапнут!Текущие характеристики:" << endl;
+					std::cout << "Двигатель свапнут!Текущие характеристики:" << endl;
 					mark.show();
 				}
 				else if (choice == 2 || mark.getls() > limit || mark.getls() == limit) {
@@ -187,12 +260,12 @@ int main() {
 				if (mark.getwheelstatus() == false) {
 					int wheelchoice = 0;
 					int wheellimit = 100;
-					cout << "Доступна замены резины на резину с большим зацепом.Заменить?" << endl;
+					std::cout << "Доступна замены резины на резину с большим зацепом.Заменить?" << endl;
 					cin >> wheelchoice;
 					if (wheelchoice == 1) {
 						mark.setwheel();
 						mark.setwhellstatus();
-						cout << "Резина заменена!Текущие характеристики:" << endl;
+						std::cout << "Резина заменена!Текущие характеристики:" << endl;
 						mark.show();
 					}
 					else if (wheelchoice == 2 || mark.setwheel() > limit || mark.setwheel() == limit) {
@@ -202,27 +275,28 @@ int main() {
 			}
 		}
 		else if (choice == 2) {
+			activeCarNode = mygarage.getCarNode(choice);
 			silvia.show();
 			int limit = 310;
 			int swap = 0;
 			if (silvia.getls() < limit) {
-				cout << "Доступен свап на SR20DET.Замена?" << endl;
+				std::cout << "Доступен свап на SR20DET.Замена?" << endl;
 				cin >> swap;
 				if (swap == 1) {
 					silvia.setLs();
-					cout << "Замена проведена успешно!Текущие характеристики:" << endl;
+					std::cout << "Замена проведена успешно!Текущие характеристики:" << endl;
 					silvia.show();
 				}
 				else if (swap == 2 || silvia.getls() > limit) {
 
 				}
 				int wheelchoice = 0;
-				cout << "Доступна замена резины на резину с большим зацепом!Замена?" << endl;
+				std::cout << "Доступна замена резины на резину с большим зацепом!Замена?" << endl;
 				cin >> wheelchoice;
 				if (wheelchoice == 1 && silvia.getwheelstatus() == false) {
 					silvia.setwheel();
 					silvia.setwhellstatus();
-					cout << "Замена прошла успешно!Текущий статус:" << endl;
+					std::cout << "Замена прошла успешно!Текущий статус:" << endl;
 					silvia.show();
 				}
 				else if (wheelchoice == 2 || silvia.getwheelstatus() == true) {
@@ -231,15 +305,16 @@ int main() {
 			}
 		}
 		else if (choice == 3) {
+			activeCarNode = mygarage.getCarNode(choice);
 			skyline.show();
 			int limit = 340;
 			int swap = 0;
 			if (skyline.getls() < limit) {
-				cout << "Доступен свап на RB26DETT.Замена?" << endl;
+				std::cout << "Доступен свап на RB26DETT.Замена?" << endl;
 				cin >> swap;
 				if (swap == 1) {
 					skyline.setLs();
-					cout << "Текущие характеристики:" << endl;
+					std::cout << "Текущие характеристики:" << endl;
 					skyline.show();
 				}
 			}
@@ -249,12 +324,12 @@ int main() {
 			int wheellimit = 100;
 			int wheelchoice = 0;
 			if (skyline.getwheel() < wheellimit && skyline.getwheelstatus() == false) {
-				cout << "Доступна замена шин на шины с большим зацепом.Замена?" << endl;
+				std::cout << "Доступна замена шин на шины с большим зацепом.Замена?" << endl;
 				cin >> wheelchoice;
 				if (wheelchoice == 1) {
 					skyline.setwheel();
 					skyline.setwhellstatus();
-					cout << "Замена прошла успешна!Текущие характеристики:" << endl;
+					std::cout << "Замена прошла успешна!Текущие характеристики:" << endl;
 					skyline.show();
 				}
 				else if (wheelchoice == 2 || skyline.getwheel() > wheellimit || skyline.getwheel() == wheellimit) {
@@ -266,9 +341,37 @@ int main() {
 			shop = false;
 		}
 	}
+
+	Part turbine("Турбина", 30000, 70);
+	Part intercooler("Интеркуллер", 25000, 50);
+	Part spoiler("Спойлер", 8000, 0);
+
+	tuningShop magaz;
+	magaz.addToEnd(&turbine);
+	magaz.addToEnd(&intercooler);
+	magaz.addToEnd(&spoiler);
+	int choicenext = 0;
+	std::cout << "Войти в магазин?" << endl;
+	cin>> choicenext;
+	if (choicenext == 1) {
+		magaz.showShop();
+		int tunchoice = 0;
+		std::cout << "Введите номер детали для покупки" << endl;
+		cin >> tunchoice;
+		Part* chosenPart = magaz.getPartPtr(tunchoice);
+		if (chosenPart != nullptr) {
+			activeCarNode->carptr->setLs(chosenPart->partBoost);
+			std::cout << "Деталь успешно установлена" << endl;
+			std::cout << "Текущие характеристики:" << endl;
+			activeCarNode->carptr->show();
+		}
+		else {
+			std::cout << "Такая деталь отсутсвует" << endl;
+		}
+	}
 	int trassa = rand() % 3 + 1;
 	int race = 0;
-	cout << "Устроить гонку?" << endl;
+	std::cout << "Устроить гонку?" << endl;
 	cin >> race;
 	if (race == 1) {
 		car* racerone = nullptr;
@@ -278,7 +381,7 @@ int main() {
 		double maxf = 0;
 		car* winner;
 		int racechoice = 0;
-		cout << "Кто на первой полосе?" << endl;
+		std::cout << "Кто на первой полосе?" << endl;
 		cin >> racechoice;
 		if (racechoice == 1) {
 			racerone = &mark;
@@ -290,7 +393,7 @@ int main() {
 			racerone = &skyline;
 		}
 		int racechoicesec = 0;
-		cout << "Кто на второй полосе?" << endl;
+		std::cout << "Кто на второй полосе?" << endl;
 		cin >> racechoicesec;
 		if (racechoicesec == 1) {
 			racertwo = &mark;
@@ -302,7 +405,7 @@ int main() {
 			racertwo = &skyline;
 		}
 		int racerchoicethree = 0;
-		cout << "Кто на третьей полосе?" << endl;
+		std::cout << "Кто на третьей полосе?" << endl;
 		cin >> racerchoicethree;
 		if (racerchoicethree == 1) {
 			racerthree = &mark;
@@ -314,7 +417,7 @@ int main() {
 			racerthree = &skyline;
 		}
 		if (trassa == 1 ) {
-			cout << "На светофоре встали" << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
+			std::cout << "На светофоре встали" << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
 			if ((double)racerone->getls() / (double)racerone->getkg() > (double)racertwo->getls() / (double)racertwo->getkg()) {
 				maxf = (double)racerone->getls() / (double)racerone->getkg();
 				winner = racerone;
@@ -327,11 +430,11 @@ int main() {
 				winner = racerthree;
 			}
 			else {
-				cout << "Победитель " << winner->getname() << endl;
+				std::cout << "Победитель " << winner->getname() << endl;
 			}
 		}
 		else if (trassa == 2) {
-			cout << "На спуске с горы стоят:" << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
+			std::cout << "На спуске с горы стоят:" << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
 			if (racerone->getls() < racertwo->getls()) {
 				max = racerone->getls();
 				winner = racerone;
@@ -341,10 +444,10 @@ int main() {
 				winner = racertwo;
 			}
 			if (max < racerthree->getls()) {
-				cout << "Победитель " << winner->getname() << endl;
+				std::cout << "Победитель " << winner->getname() << endl;
 			}
 			else {
-				cout << "Победитель " << racerthree->getname() << endl;
+				std::cout << "Победитель " << racerthree->getname() << endl;
 			}
 			vector<vector<string>>laurel_events = {
 						{"полумертвый ","Лаурель ","чуть не въехал"},
@@ -352,13 +455,13 @@ int main() {
 						{"слишком низкий ","Лаурель ",",улетел в кусты"}
 			};
 			int random_laurel = rand() % 3;
-			cout << "Во время гонки мимо прнесся "
+			std::cout << "Во время гонки мимо прнесся "
 				<< laurel_events[random_laurel][0]
 				<< laurel_events[random_laurel][1]
 				<< laurel_events[random_laurel][2]<< endl;
 		}
 		else if (trassa == 3) {
-			cout << "Гонка по трассе после дождя,на старте стоят: " << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
+			std::cout << "Гонка по трассе после дождя,на старте стоят: " << racerone->getname() << " , " << racertwo->getname() << "и" << racerthree->getname() << endl;
 			if (racerone->getls() * racerone->getwheel() > racertwo->getls() * racertwo->getwheel()) {
 				max = racerone->getls() * racerone->getwheel();
 				winner = racerone;
@@ -368,37 +471,15 @@ int main() {
 				winner = racertwo;
 			}
 			if (max > racerthree->getls() * racerthree->getwheel()) {
-				cout << "Победил " << winner->getname() << endl;
+				std::cout << "Победил " << winner->getname() << endl;
 			}
 			else {
-				cout << "Победил" << racerthree->getname() << endl;
+				std::cout << "Победил" << racerthree->getname() << endl;
 			}
 		}
 	}
-	string det1name = "Турбина";
-	int det1_price = 30000;
-	int det1_har = 50;
-	string det2name = "Интеркуллер";
-	int det2_price = 25000;
-	int det2_har = 30;
-	string det3name = "Спойлер";
-	int det3_price = 8000;
-	int det3_har = 0;
-	int tunchoice;
-	cout << "Введите деталь для покупки" << endl;
-	cin >> tunchoice;
-	vector<vector<string>>tuning;
-	tuning.push_back({ det1name, to_string(det1_price),"+" + to_string(det1_har) + "ls"});
-	tuning.push_back({ det2name, to_string(det2_price), "+" + to_string(det2_har) + "ls"});
-	tuning.push_back({ det3name, to_string(det3_price),"+" + to_string(det3_har) + "ls"});
-	if (tunchoice >= 1 && tunchoice <= 3) {
-		int ind = tunchoice - 1;
-			cout << "Деталь:"<<tuning[ind][0]
-				<<"Стоимость:"<< tuning[ind][1]
-				<<"Харакетристики:"<<tuning[ind][2] << endl;
-	}
 	mygarage.leader();
-	cout << endl;
+	std::cout << endl;
 	mygarage.lightest();
 	system("Pause");
 	return 0;
